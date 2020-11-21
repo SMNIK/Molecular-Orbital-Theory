88d3f4ff4565e3c1d0d461aa75b504b32ff7344b
SMNIK <masood.milestone@studio.unibo.it>
2020-11-21 14:11:11 -0800

    Update Benzene.py

--- molecules/Benzene.py
+++ molecules/Benzene.py
@@ -11,7 +11,7 @@ from Huckel import Molecule
 benzene = Molecule('Benzene', np.matrix([]), 6, 6, 3)
 benzene.generate_H()
 benzene.add_connections([[1,6]])
-benzene.set_constant(0,-1)
+benzene.set_constants(0,-1)
 benzene.generate_eigen()
 benzene.find_deloc_energy()
 benzene.energy_level_plot()
