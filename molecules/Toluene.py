1f1e7e86686290c75756dcba6ba3ecc686c81b78
SMNIK <masood.milestone@studio.unibo.it>
2020-11-21 12:43:02 -0800

    nice

--- molecules/Toluene.py
+++ molecules/Toluene.py
@@ -6,8 +6,15 @@ Created on Sat Nov 21 12:36:57 2020
 """
 
 import numpy as np
-from Huckel import Molecule, a, b
+from Huckel import Molecule
 
 tol = Molecule('Toluene', np.matrix([]), 7, 7, 3)
 tol.generate_H()
-tol.
+tol.add_connections([[1, 6]])
+tol.set_constants(0, -1)
+tol.generate_eigen()
+tol.find_deloc_energy()
+tol.energy_level_plot()
+tol.find_charge_density()
+tol.find_bond_order()
+print(tol)
