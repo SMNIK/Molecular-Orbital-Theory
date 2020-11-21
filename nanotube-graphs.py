111b1337c0fcd4c4eb44a66eae502e4f1f048eca
SMNIK <masood.milestone@studio.unibo.it>
2020-11-21 11:57:42 -0800

    Update nanotube-graphs.py

--- nanotube-graphs.py
+++ nanotube-graphs.py
@@ -8,3 +8,8 @@ Created on Sat Nov 21 11:54:44 2020
 from nanotube import arm
 
 arm.plot_lattice(0)
+arm.plot_lattice(1)
+arm.plot_lattice(2)
+arm.plot_lattice(3)
+arm.plot_lattice(4)
+arm.plot_lattice(5)
