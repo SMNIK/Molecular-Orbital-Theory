3c6e28c217556263fcea9909f34805b11bcab54f
SMNIK <masood.milestone@studio.unibo.it>
2020-11-21 11:43:51 -0800

    Create nanotube.py

---
+++ nanotube.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 21 11:40:36 2020
+
+@author: masou
+"""
+
+from Armchair import arm
+
+# wrap this yp in nanotube
+arm.add_connections([[1,12],[23,34],[35,42]])
+
+print(arm)
