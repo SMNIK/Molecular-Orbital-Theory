2927d18f7b3c654ecc7f055a2f9cbe02d9e45450
SMNIK <masood.milestone@studio.unibo.it>
2020-11-21 12:36:29 -0800

    Create Napthalene.py

---
+++ molecules/Napthalene.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 21 12:31:25 2020
+
+@author: masou
+"""
+
+import numpy as np
+from Huckel import Molecule, a, b
+
+napth = Molecule('Napthalene', np.matrix([]), 10, 10, 5)
+napth.generate_H()
+napth.add_connections([[5, 10], [1, 6]])
+napth.set_constants(0, -1)
+napth.generate_eigen()
+napth.find_deloc_energy()
+napth.energy_level_plot()
+napth.find_charge_density()
+napth.find_bond_order()
+print(napth)
