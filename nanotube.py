26c6af75152f9bc9dfe0cdd6edee1e617ce8604e
SMNIK <masood.milestone@studio.unibo.it>
2020-11-21 11:46:20 -0800

    Update nanotube.py

--- nanotube.py
+++ nanotube.py
@@ -9,5 +9,11 @@ from Armchair import arm
 
 # wrap this yp in nanotube
 arm.add_connections([[1,12],[23,34],[35,42]])
+arm.delete_connections([[34,35],[37,38],[39,40]])
+arm.set_connections(0,1)
+arm.generate_eigen()
+arm.find_charge_density()
+arm.find_deloc_energy()
+arm.generate_carbons(3,3)
 
 print(arm)
