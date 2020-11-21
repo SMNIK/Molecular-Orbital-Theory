9489fa579c85260490eacea8378dc1a6eaeebda4
SMNIK <masood.milestone@studio.unibo.it>
2020-11-21 12:30:42 -0800

    Update Butadiene.py

--- molecules/Butadiene.py
+++ molecules/Butadiene.py
@@ -13,3 +13,8 @@ butadiene = Molecule('Butadiene', butadiene_H, 4, 4, 2)
 
 butadiene.set_constants(0, -1)
 butadiene.generate_eigen()
+butadiene.find_deloc_energy()
+butadiene.energy_level_plot()
+butadiene.find_charge_density()
+butadiene.find_bond_order()
+print(butadiene)
