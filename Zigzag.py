e493fe1762f108aea3ad3f94691ffdd35e7e473b
SMNIK <masood.milestone@studio.unibo.it>
2020-11-21 10:52:21 -0800

    Update Zigzag.py

--- Zigzag.py
+++ Zigzag.py
@@ -4,5 +4,9 @@ Created on Sat Nov 21 10:46:28 2020
 
 @author: masou
 """
-from Graphene import Graphene
+from Graphene import Graphene as gra
 import numpy as np
+
+zig = gra('ZigZag', np.matrix([]), 48, 48, 18)
+zig.generate_H()
+
