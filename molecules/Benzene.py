9a2cfcf168757ce13efaf316a570e81e97457761
SMNIK <masood.milestone@studio.unibo.it>
2020-11-21 12:09:51 -0800

    Update Benzene.py

--- molecules/Benzene.py
+++ molecules/Benzene.py
@@ -6,3 +6,15 @@ Created on Sat Nov 21 12:02:07 2020
 """
 
 import numpy as np
+from Huckel import Molecule
+
+benzene = Molecule('Benzene', np.matrix([]), 6, 6, 3)
+benzene.generate_H()
+benzene.add_connections([[1,6]])
+benzene.set_constant(0,-1)
+benzene.generate_eigen()
+benzene.find_deloc_energy()
+benzene.energy_level_plot()
+benzene.find_charge_density()
+benzene.find_bond_order()
+print(benzene)
