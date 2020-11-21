e81ffca7b46631728912c873669e0d14319d620c
SMNIK <masood.milestone@studio.unibo.it>
2020-11-21 11:53:50 -0800

    Update Zigzag-graphs.py

--- Zigzag-graphs.py
+++ Zigzag-graphs.py
@@ -8,3 +8,8 @@ Created on Sat Nov 21 11:47:49 2020
 from Zigzag import zig
 
 zig.plot_lattice(0)
+zig.plot_lattice(1)
+zig.plot_lattice(2)
+zig.plot_lattice(3)
+zig.plot_lattice(4)
+zig.plot_lattice(5)
