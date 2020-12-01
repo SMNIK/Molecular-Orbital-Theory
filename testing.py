61e4b3e0476c4d5f155efea3a39c8e6d8529c7f0
SMNIK <masood.milestone@yahoo.com>
2020-12-01 10:40:48 -0800

    Update testing.py

--- testing.py
+++ testing.py
@@ -10,3 +10,15 @@ from Molecule import Molecule
 class Test(unittest.TestCase):
     def test(self):
         assert Molecule
+        assert Molecule.find_bond_order
+        assert Molecule.find_deloc_energy
+        assert Molecule.find_charge_density
+        assert Molecule.find_nodes
+        assert Molecule.set_constants
+        assert Molecule.generate_eigen
+        assert Molecule.generate_H
+        assert Molecule.energy_level_plot
+        assert Molecule.delete_connections
+        assert Molecule.add_connections
+        assert Molecule.__str__
+        assert Molecule.__init__
