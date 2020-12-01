855bd79bd86769c85dc66b46b7d4021aa574de1b
SMNIK <masood.milestone@yahoo.com>
2020-12-01 09:18:36 -0800

    Create testing.py

---
+++ testing.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec  1 09:15:33 2020
+
+@author: masou
+"""
+
+import unittest
+from Molecule import Molecule
+class Test(unittest.TestCase):
+    def test(self):
+        assert Molecule
