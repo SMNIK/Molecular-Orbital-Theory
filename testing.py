a13be9f2d565e57d3ddd63673deedc0772d227c9
SMNIK <seyedmasood.nikkhah@studio.unibo.it>
2021-01-14 17:56:13 -0800

    Update testing.py

--- testing.py
+++ testing.py
@@ -4,9 +4,10 @@ Created on Tue Dec  1 09:15:33 2020
 
 @author: masou
 """
-
-import unittest
 from Molecule import Molecule
+import unittest
+
+
 class Test(unittest.TestCase):
     def test(self):
         assert Molecule
