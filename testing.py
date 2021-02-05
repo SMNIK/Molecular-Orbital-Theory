bbf4e9dcaecbd4e4f3efa339b82470fc00ce781f
SMNIK <masood.milestone@yahoo.com>
2021-02-05 13:29:23 -0800

    Update testing.py

--- testing.py
+++ testing.py
@@ -18,12 +18,12 @@ class TestMolecule(unittest.TestCase):
 
     @classmethod
     def setDownClass(cls):
-        print('tear down class Molecule')
+        print('tear down class Molecule\n')
 
     def setUp(self):
         print('set up Molecule')
         self.valid = Molecule("testing", np.matrix([]), 4, 4, 2)
-        selv.valid.generate_H()
+        self.valid.generate_H()
         self.valid.add_connections([[1, 4]])
         self.valid.set_constants(0, -1)
         self.valid.generate_eigen()
@@ -31,6 +31,17 @@ class TestMolecule(unittest.TestCase):
         self.valid.energy_level_plot()
         self.valid.find_charge_density()
         self.valid.find_bond_order()
+
+    @classmethod
+    def tearDown(cls):
+        print('tear down Molecule\n')
+
+    def test_set_constants(self):
+        print('test set constants')
+        Molecule.H = np.matrix([[0, -1], [-1, 0]])
+        self.H = np.matrix([])
+        Molecule.set_constants(0, -1)
+        self.assertEqual(str(Molecule.H), '[[ 0 -1]\n [-1  0]]')
 
 
 if __name__ == '__main__':
