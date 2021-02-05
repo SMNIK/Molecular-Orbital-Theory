1db3d3dca4e21209e6d2a269ca237a4701b526c5
SMNIK <masood.milestone@yahoo.com>
2021-02-05 13:22:35 -0800

    Update testing.py

--- testing.py
+++ testing.py
@@ -10,18 +10,27 @@ import unittest
 from Molecule import Molecule
 
 
-class TestBenzene(unittest.TestCase):
+class TestMolecule(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        print('set up class Benzene')
+        print('set up class Molecule')
 
     @classmethod
     def setDownClass(cls):
-        print('tear down class Benzene')
+        print('tear down class Molecule')
 
     def setUp(self):
-        print('set up Benzene')
+        print('set up Molecule')
+        self.valid = Molecule("testing", np.matrix([]), 4, 4, 2)
+        selv.valid.generate_H()
+        self.valid.add_connections([[1, 4]])
+        self.valid.set_constants(0, -1)
+        self.valid.generate_eigen()
+        self.valid.find_deloc_energy()
+        self.valid.energy_level_plot()
+        self.valid.find_charge_density()
+        self.valid.find_bond_order()
 
 
 if __name__ == '__main__':
